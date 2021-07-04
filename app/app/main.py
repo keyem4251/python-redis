84119c44aaa523f3da13a2864c99f64e54b7bf80
keyem4251 <kisk.emura@gmail.com>
2021-07-04 18:21:31 +0900

    redis pipeline

--- app/app/main.py
+++ app/app/main.py
@@ -8,10 +8,13 @@ HOST = os.getenv("HOST", "localhost")
 
 
 def main():
-    client = redis.Redis(host=HOST, port=PORT)
-    client.set("key1", "abc")
-    value = client.get("key1")
-    print(value)
+    r = redis.Redis(host=HOST, port=PORT)
+    with r.pipeline() as pipe:
+        pipe.set("key1", "abc")
+        pipe.hmset("product_1", {
+            "color": "green",
+            "price": 100,
+        })
 
 
 if __name__ == "__main__":
